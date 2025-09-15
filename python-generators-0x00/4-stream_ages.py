a30d047c11afa611c63548447b8ccbcdb9008349
Chinemerem Okonkwo <gentycool@yahoo.com>
2025-09-15 17:41:03 +0100

    Tried task 4

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,10 +1,10 @@
-def stream_user_ages(data_set):
+def stream_user_ages():
     for a in data_set:
         yield age
 
 def AVERAGE():
     a, c = 0, 0 #a is current average, c is count of the number of ages
-    for x in stream_user_ages(data_set):
+    for x in stream_user_ages():
         c += 1
         a = (a * (c - 1)) + x) // c
     return a
